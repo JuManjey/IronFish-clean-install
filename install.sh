3090aa26544a987cd9bf3017f57895af6c21b564
JuManjey <opghld@gmail.com>
2021-12-12 15:48:49 +0200

    main

--- install.sh
+++ install.sh
@@ -1,9 +1,11 @@
+#update
 apt-get update && apt-get upgrade
 wget -q -O ironfish.sh https://api.nodes.guru/ironfish.sh && chmod +x ironfish.sh && sudo /bin/bash ironfish.sh
-#sdkfmnlsdf sdf sdf
-apt-get update && apt-get upgrade
-wget -q -O ironfish.sh https://api.nodes.guru/ironfish.sh && chmod +x ironfish.sh && sudo /bin/bash ironfish.shapt-get update && apt-get upgrade
-wget -q -O ironfish.sh https://api.nodes.guru/ironfish.sh && chmod +x ironfish.sh && sudo /bin/bash ironfish.shapt-get update && apt-get upgrade
-wget -q -O ironfish.sh https://api.nodes.guru/ironfish.sh && chmod +x ironfish.sh && sudo /bin/bash ironfish.shapt-get update && apt-get upgrade
-wget -q -O ironfish.sh https://api.nodes.guru/ironfish.sh && chmod +x ironfish.sh && sudo /bin/bash ironfish.shapt-get update && apt-get upgrade
-wget -q -O ironfish.sh https://api.nodes.guru/ironfish.sh && chmod +x ironfish.sh && sudo /bin/bash ironfish.sh
+#Load variables
+. $HOME/.bashrc
+. $HOME/.bash_profile
+#check the config
+cat $HOME/.ironfish/config.json
+#set the NODE name
+ironfish config:set nodeName $IRONFISH_NODENAME
+ironfish config:set blockGraffiti $IRONFISH_NODENAME
